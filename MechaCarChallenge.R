730bcb0423bd87fbc9ead72fffe4c1da4e6a272d
andgerashchenko <79814533+andgerashchenko@users.noreply.github.com>
2021-08-07 16:15:24 -0400

    Suspension Coils analysis added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,8 @@ library(dplyr)
 mechacar_mpg <- read.csv(file='MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg))
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv')
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
